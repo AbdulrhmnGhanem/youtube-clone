2b1c6a30b1e29f742fdb7a5804b050bd78ec6c07
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2019-09-04 00:48:32 +0200

    final requirements

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './VideoItem.css';
 
-const VideoItem = ({video}) => {
+const VideoItem = ({video, onVideoSelect}) => {
     return (
-        <div className={"video-item item"}>
-            <img className={"ui image"} src={video.snippet.thumbnails.medium.url}/>
+        <div className={"video-item item"} onClick={() => onVideoSelect(video)}>
+            <img alt={video.snippet.title} className={"ui image"} src={video.snippet.thumbnails.medium.url}/>
             <div className={"content"}>
                 <div className={"header"}>
                     {video.snippet.title}
